7cb36ce0cbb7efef72882ee12f98012f10dda6af
Lilly Piri <lilly@lillypiri.com>
2017-08-25 12:09:59 +1000

    when other is selected, it adds an input field

---
+++ js/app.js
@@ -0,0 +1,29 @@
+var fieldset = document.getElementsByTagName('fieldset')[0];
+var addedField = document.createElement("INPUT");
+
+// when other is selected, it calls the addOtherField function
+document.addEventListener('DOMContentLoaded',function() {
+    document.querySelector('select[name="user_title"]').onchange=changeEventHandler;
+},false);
+
+function changeEventHandler(event) {
+    if(event.target.value === 'other') {
+        console.log('Other selected');
+        addOtherField();
+    } else {
+        console.log('You are a ' + event.target.value + '.');
+            fieldset.removeChild(addedField);
+    }
+}
+
+
+
+//TODO when other is selected you have to check if the extra input is already there
+// fix dom error
+
+// adds input field when 'other' job title is selected
+function addOtherField() {
+    addedField.setAttribute("type", "text");
+    addedField.setAttribute("class", "added-field");
+    fieldset.appendChild(addedField);
+}
