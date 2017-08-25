1f2dd61cbe0cf3e994bec2e21ea77694b0c244da
Lilly Piri <lilly@lillypiri.com>
2017-08-25 16:18:18 +1000

    When we choose a payment option, the chosen payment section is revealed and the other payment sections are hidden

--- js/app.js
+++ js/app.js
@@ -36,13 +36,27 @@ function addOtherField() {
 // No color options appear in the “Color” menu until the user chooses a T-Shirt theme. The “Color” menu reads “Please select a T-shirt theme” until a theme is selected from the “Design” menu.
 
 // When we choose a payment option, the chosen payment section is revealed and the other payment sections are hidden
+
+
+function hidePaymentFields() {
+    document.getElementById("credit-card").style.display = "none";
+    document.getElementById("bitcoin-div").style.display = "none";
+    document.getElementById("paypal-div").style.display = "none";
+}
+hidePaymentFields();
+
 document.addEventListener('DOMContentLoaded',function() {
     document.querySelector('select[name="user_payment"]').onchange=changePaymentHandler;
 },false);
 
 function changePaymentHandler(event) {
+    hidePaymentFields();
     console.log(event.target.value);
-    if (event.target.value !== 'credit card') {
-        document.getElementById("credit-card").style.display = "none";
+    if (event.target.value === 'credit card') {
+        document.getElementById("credit-card").style.display = "block";
+    } else if (event.target.value === 'bitcoin') {
+        document.getElementById("bitcoin-div").style.display = "block";
+    } else if (event.target.value === 'paypal') {
+        document.getElementById("paypal-div").style.display = "block";
     }
 }
