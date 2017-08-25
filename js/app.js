e871f8bfca08582ceb97bfb79bb5de8db852889b
Lilly Piri <lilly@lillypiri.com>
2017-08-25 15:02:32 +1000

    when a user chooses anything other than credit card payment option, the credit card form is hidden

--- js/app.js
+++ js/app.js
@@ -22,11 +22,27 @@ function changeEventHandler(event) {
 
 
 //TODO when other is selected you have to check if the extra input is already there
-// fix dom error
+
 
 // adds input field when 'other' job title is selected
 function addOtherField() {
     addedField.setAttribute("type", "text");
     addedField.setAttribute("class", "added-field");
     fieldset.appendChild(addedField);
+}
+
+
+
+// No color options appear in the “Color” menu until the user chooses a T-Shirt theme. The “Color” menu reads “Please select a T-shirt theme” until a theme is selected from the “Design” menu.
+
+// When we choose a payment option, the chosen payment section is revealed and the other payment sections are hidden
+document.addEventListener('DOMContentLoaded',function() {
+    document.querySelector('select[name="user_payment"]').onchange=changePaymentHandler;
+},false);
+
+function changePaymentHandler(event) {
+    console.log(event.target.value);
+    if (event.target.value !== 'credit card') {
+        document.getElementById("credit-card").style.display = "none";
+    }
 }
